412208c47a9900e5fc573e649271d100253f3190
Carl Hu <shuohu@andrew.cmu.edu>
2023-12-05 09:13:58 -0500

    update

--- cam_vis.py
+++ cam_vis.py
@@ -28,25 +28,10 @@ from phalp.utils import get_pylogger
 from phalp.configs.base import CACHE_DIR
 from hmr2.utils.renderer import Renderer, cam_crop_to_full
 
-<<<<<<< HEAD
 from hmr2.datasets.utils import expand_bbox_to_aspect_ratio
 from hmr2.datasets.vitdet_dataset import ViTDetDataset, DEFAULT_MEAN, DEFAULT_STD
 import mediapipe as mp
 import neural_renderer as nr
-=======
-# from hmr2.datasets.utils import expand_bbox_to_aspect_ratio
-#from hmr2.datasets.vitdet_dataset import ViTDetDataset, DEFAULT_MEAN, DEFAULT_STD
-
-
-
-from hmr2.datasets.utils import (convert_cvimg_to_tensor,
-                    expand_to_aspect_ratio,
-                    generate_image_patch_cv2)
-
-
-from hmr2.models import load_poselift
-
->>>>>>> 954b0d7d7b78d91f144b45205c2ce0993c42eacf
 from ultralytics import YOLO
 
 
@@ -75,8 +60,6 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 
 def main(): 
 
-    global model, device
-
     width = 640
     height = 480
 
@@ -86,26 +69,8 @@ def main():
     config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
     pipeline.start(config)
 
-<<<<<<< HEAD
     # bounding_boxes = []
     # pose_keypoints_2d = []
-=======
-    # cfg = Human4DConfig()
-    # hmr_traker = HMR2_4dhuman(cfg)
-
-    bounding_boxes = []
-    #pose_keypoints_2d = []
-    pose_keypoints_2d = np.zeros((25, 3))
-
-    # # ------------------default check point----------
-    # parser = argparse.ArgumentParser(description='HMR2 demo code')
-    # parser.add_argument('--out_folder', type=str, default='demo_pose_out', help='Output folder to save rendered results')
-    # parser.add_argument('--checkpoint', type=str, default=Poselift_CHECKPOINT, help='Path to pretrained model checkpoint')
-    # args = parser.parse_args()
-    # # Download and load checkpoints
-    # download_models(CACHE_DIR_4DHUMANS)
-    # model, model_cfg = load_poselift(args.checkpoint)
->>>>>>> 954b0d7d7b78d91f144b45205c2ce0993c42eacf
 
     # ------------------default check point----------
     parser = argparse.ArgumentParser(description='HMR2 demo code')
@@ -144,14 +109,8 @@ def main():
 
         box_image = color_image.copy()
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
         # output_test = model(test_dict)
 
-=======
->>>>>>> 954b0d7d7b78d91f144b45205c2ce0993c42eacf
->>>>>>> Stashed changes
         #1. Yolo outputs 2d pose, bbox
         all_verts = []
         all_cam_t = []
@@ -238,7 +197,8 @@ def main():
                     # -----------------------------boxes-----------------
                     
                     x1, y1, x2, y2 = boxes[0,0], boxes[0,1], boxes[0,2], boxes[0,3]
-                    box_center = y
+                    box_center = (x2-x1)//2
+                    
                     
                     
                     test_dict = {
@@ -264,70 +224,14 @@ def main():
                                 # x, y = int(x), int(y)  # Convert to integers
                                 # print(x, y, prob)
 
-<<<<<<< HEAD
                                 # if max_prob_box[0] <= x <= max_prob_box[2] and max_prob_box[1] <= y <= max_prob_box[3]:
                                 #     # Draw the keypoint only if it is within the bounding box
                                 #     #pose_keypoints_2d = np.concatenate((pose_keypoints_2d, [[x, y]]), axis=0)
                                 #     cv2.circle(box_image, (x, y), 2, (0, 0, 255), -1)
-=======
-                                if max_prob_box[0] <= x <= max_prob_box[2] and max_prob_box[1] <= y <= max_prob_box[3]:
-                                    # Draw the keypoint only if it is within the bounding box
-                                    #pose_keypoints_2d = np.concatenate((pose_keypoints_2d, [[x, y]]), axis=0)
-                                    cv2.circle(box_image, (x, y), 2, (0, 0, 255), -1)
-<<<<<<< Updated upstream
-=======
->>>>>>> 954b0d7d7b78d91f144b45205c2ce0993c42eacf
->>>>>>> Stashed changes
 
                                 # print(keypoints.shape)
                                 # print(keypoints)
                                 #17 points
-<<<<<<< Updated upstream
-                                pose_keypoints_2d = keypoints[0, :, :2]
-                                # print(pose_keypoints_2d.shape)
-                                # print(pose_keypoints_2d)
-
-                else:
-                    continue
-            
-
-            pose_keypoints_2d = np.asarray(pose_keypoints_2d).astype(np.float32)
-
-            img_patch = convert_cvimg_to_tensor(color_image)
-
-            default_mean = np.array([0.485, 0.456, 0.406])
-            default_std = np.array([0.229, 0.224, 0.225])
-
-
-            for n_c in range(min(color_image.shape[2], 3)):
-                img_patch[n_c, :, :] = (img_patch[n_c, :, :] - default_mean[n_c]) / default_std[n_c]
-        
-            img_patch = torch.tensor(img_patch).float().unsqueeze(0)
-            pose_keypoints_2d = torch.tensor(pose_keypoints_2d).float().unsqueeze(0)
-
-            # apply normalization
-
-            item = {
-                'input_keypoints_2d': pose_keypoints_2d,
-                'img': img_patch,
-                'personid': 1,
-            }
-            
-            #numbers are for testing. 
-            item['box_center'] = 3
-            item['box_size'] = 2
-            item['img_size'] = 1.0 * np.array([color_image.shape[1], color_image.shape[0]])
-                        
-                
-            # dataloader = torch.utils.data.DataLoader(item, batch_size=1, shuffle=False, num_workers=0)
-
-            # print(dataloader)
-
-            item = recursive_to(item, device)
-            with torch.no_grad():
-                out = model(item)
-=======
-<<<<<<< HEAD
                                 
                                 # pose_keypoints_2d = keypoints[0, :, :2]
                                 # print(pose_keypoints_2d)
@@ -349,52 +253,6 @@ def main():
                 #     bounding_boxes.append(box)
                 #     #print(box)
                 #     cls_id = box[-1]
-=======
-                                pose_keypoints_2d = keypoints[0, :, :2]
-                                # print(pose_keypoints_2d.shape)
-                                # print(pose_keypoints_2d)
->>>>>>> Stashed changes
-
-                else:
-                    continue
-            
-
-            pose_keypoints_2d = np.asarray(pose_keypoints_2d).astype(np.float32)
-
-            img_patch = convert_cvimg_to_tensor(color_image)
-
-            default_mean = np.array([0.485, 0.456, 0.406])
-            default_std = np.array([0.229, 0.224, 0.225])
-
-
-            for n_c in range(min(color_image.shape[2], 3)):
-                img_patch[n_c, :, :] = (img_patch[n_c, :, :] - default_mean[n_c]) / default_std[n_c]
-        
-            img_patch = torch.tensor(img_patch).float().unsqueeze(0)
-            pose_keypoints_2d = torch.tensor(pose_keypoints_2d).float().unsqueeze(0)
-
-            # apply normalization
-
-            item = {
-                'input_keypoints_2d': pose_keypoints_2d,
-                'img': img_patch,
-                'personid': 1,
-            }
-            
-            #numbers are for testing. 
-            item['box_center'] = 3
-            item['box_size'] = 2
-            item['img_size'] = 1.0 * np.array([color_image.shape[1], color_image.shape[0]])
-                        
-                
-            # dataloader = torch.utils.data.DataLoader(item, batch_size=1, shuffle=False, num_workers=0)
-
-            # print(dataloader)
-
-            item = recursive_to(item, device)
-            with torch.no_grad():
-                out = model(item)
->>>>>>> 954b0d7d7b78d91f144b45205c2ce0993c42eacf
 
                 #     class_list = YOLO_model.model.names
                 #     class_name = class_list[cls_id]
